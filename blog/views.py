63d4d11cbd00c66c3b1ed5855ab70c41d2276a24
dsantia <54283899+dsantia@users.noreply.github.com>
2019-09-02 12:16:11 -0600

    Agregada una plantilla

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
 # Create your views here.
+def listar_pub(request):
+    pubs = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/listar_pub.html', {'pubs': pubs})
